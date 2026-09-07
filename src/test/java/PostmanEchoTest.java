ab73429874827033ee81791d030fee1a6e1555e2
danik <darkman147147@gmail.com>
2026-09-07 14:02:25 +0400

    Java homework

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,10 +1,8 @@
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
 
@@ -15,95 +13,64 @@ public class PostmanEchoTest {
 
     @Test
     void testGet() {
-        Response response = given()
+        given()
                 .when()
                 .get("/get")
                 .then()
-                .extract().response();
-
-        assertEquals(200, response.statusCode());
-        assertNotNull(response.jsonPath().getMap("args"));
-        assertNotNull(response.jsonPath().getMap("headers"));
-        assertEquals("https://postman-echo.com/get", response.jsonPath().getString("url"));
+                .log().all()                           // ← выводит весь ответ в консоль
+                .statusCode(200)
+                .body("url", equalTo("https://postman-echo.com/get"));
     }
 
     @Test
     void testPost() {
-        Response response = given()
+        given()
                 .contentType("application/json")
                 .body("{\"key\":\"value\"}")
                 .when()
                 .post("/post")
                 .then()
-                .extract().response();
-
-        assertEquals(200, response.statusCode());
-        assertNotNull(response.jsonPath().getMap("args"));
-        assertNotNull(response.jsonPath().get("data"));
-        assertNotNull(response.jsonPath().getMap("files"));
-        assertNotNull(response.jsonPath().getMap("form"));
-        assertNotNull(response.jsonPath().getMap("headers"));
-        assertNotNull(response.jsonPath().getMap("json"));
-        assertEquals("https://postman-echo.com/post", response.jsonPath().getString("url"));
+                .log().all()
+                .statusCode(200)
+                .body("url", equalTo("https://postman-echo.com/post"));
     }
 
     @Test
     void testPut() {
-        Response response = given()
+        given()
                 .contentType("application/json")
                 .body("{\"key\":\"value\"}")
                 .when()
                 .put("/put")
                 .then()
-                .extract().response();
-
-        assertEquals(200, response.statusCode());
-        assertNotNull(response.jsonPath().getMap("args"));
-        assertNotNull(response.jsonPath().get("data"));
-        assertNotNull(response.jsonPath().getMap("files"));
-        assertNotNull(response.jsonPath().getMap("form"));
-        assertNotNull(response.jsonPath().getMap("headers"));
-        assertNotNull(response.jsonPath().getMap("json"));
-        assertEquals("https://postman-echo.com/put", response.jsonPath().getString("url"));
+                .log().all()
+                .statusCode(200)
+                .body("url", equalTo("https://postman-echo.com/put"));
     }
 
     @Test
     void testPatch() {
-        Response response = given()
+        given()
                 .contentType("application/json")
                 .body("{\"key\":\"value\"}")
                 .when()
                 .patch("/patch")
                 .then()
-                .extract().response();
-
-        assertEquals(200, response.statusCode());
-        assertNotNull(response.jsonPath().getMap("args"));
-        assertNotNull(response.jsonPath().get("data"));
-        assertNotNull(response.jsonPath().getMap("files"));
-        assertNotNull(response.jsonPath().getMap("form"));
-        assertNotNull(response.jsonPath().getMap("headers"));
-        assertNotNull(response.jsonPath().getMap("json"));
-        assertEquals("https://postman-echo.com/patch", response.jsonPath().getString("url"));
+                .log().all()
+                .statusCode(200)
+                .body("url", equalTo("https://postman-echo.com/patch"));
     }
 
     @Test
     void testDelete() {
-        Response response = given()
+        given()
                 .contentType("application/json")
                 .body("{\"key\":\"value\"}")
                 .when()
                 .delete("/delete")
                 .then()
-                .extract().response();
-
-        assertEquals(200, response.statusCode());
-        assertNotNull(response.jsonPath().getMap("args"));
-        assertNotNull(response.jsonPath().get("data"));
-        assertNotNull(response.jsonPath().getMap("files"));
-        assertNotNull(response.jsonPath().getMap("form"));
-        assertNotNull(response.jsonPath().getMap("headers"));
-        assertNotNull(response.jsonPath().getMap("json"));
-        assertEquals("https://postman-echo.com/delete", response.jsonPath().getString("url"));
+                .log().all()
+                .statusCode(200)
+                .body("url", equalTo("https://postman-echo.com/delete"));
     }
 }
